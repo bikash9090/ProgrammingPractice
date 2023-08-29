7d3e8c525f1c3664d1ca670916e3fec100cd7119
Bikash kumar sethy <info.bikash9@gmail.com>
2023-08-29 16:18:49 +0530

    Power of number

--- src/logic/developmenet/program/PowerOfNumber.java
+++ src/logic/developmenet/program/PowerOfNumber.java
@@ -1,17 +1,16 @@
 package logic.developmenet.program;
 
+import java.util.Scanner;
+
 public class PowerOfNumber {
 
 	public static void main(String[] args) {
-		int base = 2;
-		int exponent = 12;
-		long result = 1;
 		
-		for(int i=0;i<exponent;i++) {
-			result *= base;
-		}
-		System.out.println(result);
-		System.out.println((int)Math.pow(base, exponent));
+		System.out.println("Enter the number to find the power of its.");
+		Scanner input = new Scanner(System.in);
+		int number = input.nextInt();
+		
+		
 	}
 
 }
